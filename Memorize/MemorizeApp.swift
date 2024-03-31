e352ecd37c8b1d6afbe32b319197acc41b7fbc7f
Justin Tran <jhtranx@gmail.com>
2024-03-30 23:09:36 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Justin Tran on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
